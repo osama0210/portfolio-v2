bc664ae0279f4ddb4a52a0a09b156b53c9f7a12f
osama0210 <osama.asmi04@gmail.com>
2025-08-28 20:28:35 +0200

    feat: started with navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,22 +1,23 @@
+import Logo from '../assets/Logo.svg';
 
 const Navbar = () => {
   return (
-    <header className="fixed">
-      <nav className="">
-        <div className="">
-            <svg></svg>
-        </div>
-          <div>
-              <ul>
-                  <li></li>
-                  <li></li>
-                  <li></li>
+      <header className="bg-primary w-full h-[90px] fixed">
+          <nav className="max-w-[1500px] mx-auto px-10 h-full flex items-center justify-between">
+              <a href="#hero">
+                  <img src={Logo} alt=""/>
+              </a>
+
+              <ul className="hidden md:flex gap-8 absolute left-1/2 -translate-x-1/2 text-white font-light">
+                  <li><a href="#about" className="hover:text-accent transition">About</a></li>
+                  <li><a href="#projects" className="hover:text-accent transition">Projects</a></li>
+                  <li><a href="#contact" className="hover:text-accent transition">Contact</a></li>
               </ul>
-          </div>
-          <div>
-              <button>Download CV</button>
-          </div>
-      </nav>
+
+              <a href="" className="hover:text-accent transition text-white font-medium">
+                  Download CV
+              </a>
+          </nav>
     </header>
   );
 };
