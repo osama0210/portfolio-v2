1c96ce27176dc056f7d6d309362cdd21db3e59bf
osama0210 <osama.asmi04@gmail.com>
2025-09-02 21:23:54 +0200

    feat(hero): add initial hero section structure and styling

--- frontend/src/sections/Hero.tsx
+++ frontend/src/sections/Hero.tsx
@@ -1,20 +1,31 @@
-const Hero = () => {
-  return (
-    <section
-      id="hero"
-      className="min-h-screen flex md:justify-center items-center px-10"
-    >
-      <div className="flex flex-col gap-3 w-fit">
-        <h1 className="text-5xl md:text-7xl text-primary md:text-center">
-          Hello, <br /> I’m Osama.
-        </h1>
-        <p className="md:text-center text-black">
-          I’m a developer focused on structured growth. <br/> I believe in discipline,
-          clarity, and building things that matter.
-        </p>
-      </div>
-    </section>
-  );
-};
+  import ArrowRight from "../assets/arrow-right.svg";
 
-export default Hero;
+  const Hero = () => {
+    return (
+      <section
+        id="hero"
+        className="min-h-screen flex md:justify-center items-center px-10 md:px-12"
+      >
+        <div className="flex flex-col gap-4 animate-fade-in max-w-screen-md">
+          <h1 className="text-6xl md:text-8xl text-primary md:text-center tracking-wide leading-none mb-3">
+            Hello, I’m <br /> Osama Alasmi.
+          </h1>
+          <p className="text-base md:text-center text-black md:text-xl font-normal">
+            I’m a developer focused on structured growth. <br /> I believe in
+            discipline, clarity, and building things that matter.
+          </p>
+          <div className="flex justify-end md:justify-center my-5">
+            <button
+              aria-label="Scroll to projects"
+              className="bg-secondary inline-flex items-center gap-2 text-white text-base  md:text-lg px-6 py-2 rounded-lg shadow-md hover:shadow-lg hover:bg-secondary/90 transition"
+            >
+              See my work
+              <img src={ArrowRight} alt="" />
+            </button>
+          </div>
+        </div>
+      </section>
+    );
+  };
+
+  export default Hero;
