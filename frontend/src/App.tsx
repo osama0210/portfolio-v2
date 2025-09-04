404fd41067fded3f3cfa4f1e9116494f47ae8784
osama0210 <osama.asmi04@gmail.com>
2025-09-04 23:59:37 +0200

    Feat(About): started with about section.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero.tsx";
+import About from "./sections/About.tsx";
 
 export default function App() {
   return (
-      <div className="">
-          <Navbar/>
-          <Hero/>
+    <div className="">
+      <Navbar />
+      <Hero />
+      <About />
     </div>
   );
 }
---
+++ frontend/src/sections/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return(
+        <section className="min-h-screen bg-primary">
+
+        </section>
+    )
+}
+
+export default About
